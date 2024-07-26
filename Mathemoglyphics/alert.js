b2d42efe7e4b16c464b173b447150ebcf2adb315
0201 <92650958+v81d@users.noreply.github.com>
2024-07-26 13:02:29 +0800

    Update alert.js

--- Mathemoglyphics/alert.js
+++ Mathemoglyphics/alert.js
@@ -49,7 +49,7 @@ function CustomAlert() {
       dialogboxhead.style.display = 'none';
     } else {
       dialogboxhead.style.display = 'block';
-      dialogboxhead.innerHTML = '<svg style="vertical-align: middle;" width="19" height="19" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2.20164 18.4695L10.1643 4.00506C10.9021 2.66498 13.0979 2.66498 13.8357 4.00506L21.7984 18.4695C22.4443 19.6428 21.4598 21 19.9627 21H4.0373C2.54022 21 1.55571 19.6428 2.20164 18.4695Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/><path d="M12 9V13" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/><path d="M12 17.0195V17" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>' + '<p style="vertical-align: middle; display: inline; margin-left: 8px;">' + title + "</p>";
+      dialogboxhead.innerHTML = '<svg style="vertical-align: middle;" width="19" height="19" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2.20164 18.4695L10.1643 4.00506C10.9021 2.66498 13.0979 2.66498 13.8357 4.00506L21.7984 18.4695C22.4443 19.6428 21.4598 21 19.9627 21H4.0373C2.54022 21 1.55571 19.6428 2.20164 18.4695Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/><path d="M12 9V13" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/><path d="M12 17.0195V17" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>' + '<p style="vertical-align: middle; display: inline-block; white-space: nowrap; margin-left: 8px;">' + title + "</p>";
     }
 
     dialogboxbody.innerHTML = message;
